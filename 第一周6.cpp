17140c616aec423350bfdfac24c80e58db8789c6
qinshuguo <qinshuguo@qq.com>
2021-12-02 23:11:46 +0800

    第一周完成

---
+++ 第一周6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int a,b;
+double c,d;
+char L1,L2; 
+int main(){
+	cout<<"请输入两个整数a和b"<<endl;
+	cin>>a>>b;
+	cout<<"请输入两个实数c和d"<<endl;
+	cin>>c>>d;
+	cout<<"请输入两个整数L1和L2"<<endl;
+	cin>>L2>>L2;
+	cout<<"a+b="<<a+b<<endl;
+	cout<<"c+d="<<c+d<<endl;
+	cout<<"L1+L2="<<L1+L2<<endl;
+	return 0; 
+} 
